87bfdfcf2660f3d2d6ef2e7e93e18dce838e61f2
Harold Fachin <harold.f415@gmail.com>
2020-08-08 11:51:07 -0400

    updated to querySelector > getElementByID

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function diceGame() {
   var randomNumber1 = Math.floor((Math.random() * 6) + 1);
   var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 
-  document.getElementsByClassName("dice")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-  document.getElementsByClassName("dice")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
   if (randomNumber1 > randomNumber2)
     document.querySelector("h1").textContent = "Player 1 Wins";
